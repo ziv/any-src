3e8d671c9138d7692dd02b1207c165675199d6f7
Ziv <ziv.perry@zoominfo.com>
2021-03-16 21:51:41 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# sourceable
+# any-src
+
+## Under the hood
+```html
+<any-src src="/path/to/json">
+    <p>Hello <span data-text="name"></span>!</p>
+</any-src>
+```
+
+Flow
+```
+attibute(src) 
+    -> hide(all) 
+    -> display(loader)
+    -> load(src)
+    -> 
+request >
+```
+1. request
--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const responseHandler = (res) => {
+    if (!res.ok) {
+        throw new Error(`Error ${res.status}`);
+    }
+    return res.json();
+}
+
+const updateFactory = (el) => (data) => (key, prop) => el
+    .querySelectorAll(`[data-${key}]`)
+    .forEach(e => e[prop] = data[e.dataset[key]]);
 
 customElements.define('any-src', class Sourceable extends HTMLElement {
     static get observedAttributes() {
@@ -9,17 +19,23 @@ customElements.define('any-src', class Sourceable extends HTMLElement {
     }
 
     attributeChangedCallback() {
+        // todo complete fetch HTTP options
+        // const http = this.dataset.http;
+        // if (http) {
+        //     // parse json and use it as fetch options
+        // }
+        // todo complete loader
+        // const loader = this.dataset.loader;
+        // if (loader) {
+        //     // handle loader display
+        // }
         fetch(this.getAttribute('src'))
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error(res.status);
-                }
-                return res.json();
-            })
-            .then(data => {
-                this.querySelectorAll('[data-text]').forEach(el => el.innerText = data[el.dataset.text]);
-                this.querySelectorAll('[data-src]').forEach(el => el.src = data[el.dataset.src]);
-                this.querySelectorAll('[data-value]').forEach(el => el.value = data[el.dataset.value]);
+            .then(responseHandler)
+            .then(updateFactory(this))
+            .then(update => {
+                update('text', 'innerText');
+                update('src', 'src');
+                update('value', 'value');
             });
     }
 });
